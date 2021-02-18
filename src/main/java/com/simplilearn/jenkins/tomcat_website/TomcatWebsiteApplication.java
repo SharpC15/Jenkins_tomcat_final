9f16db323ddd8a477ea738b6da14c4fc9c276483
student <chastin.davis@ngc.com>
2021-02-18 02:00:15 +0000

    first commit

---
+++ src/main/java/com/simplilearn/jenkins/tomcat_website/TomcatWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.jenkins.tomcat_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/jenkins/tomcat_website/TomcatWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.jenkins.tomcat_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
